0c770d096f0db9bf36d79454fbad84ac08a7f13c
Adriano Souza Costa <adriano.souza@prodoctor.net>
2023-08-21 13:56:14 -0300

    Created ads components

---
+++ src/app/components/ads/300x400/index.js
@@ -0,0 +1,11 @@
+'user client'
+
+import styles from './300x400.module.css'
+
+export default function Banner300x400() {
+    return (
+        <div className={styles.container}>
+            300x400
+        </div>
+    )
+}
---
+++ src/app/components/ads/970x90/index.js
@@ -0,0 +1,11 @@
+'user client'
+
+import styles from './970x90.module.css'
+
+export default function Banner970x90() {
+    return (
+        <div className={styles.container}>
+            970x100
+        </div>
+    )
+}
