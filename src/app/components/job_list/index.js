d0dac6008696818be1ada0a6ecc3fab33030fb15
Adriano Souza Costa <adriano.souza@prodoctor.net>
2023-08-21 10:14:44 -0300

    Add style in job list items

--- src/app/components/job_list/index.js
+++ src/app/components/job_list/index.js
@@ -1,19 +1,57 @@
+'user client'
+import Image from 'next/image'
+import Link from 'next/link'
+
 import styles from './job_list.module.css'
+
+function JobListItem() {
+    return (
+        <div className={styles.job_list_item}>
+            <div className={styles.company_wrapper}>
+                <Image
+                    src="https://assets.website-files.com/60c77302fcfa2bdb6e595f76/60c7c7ea645b46e9836da40c_icon-4-company-job-board-x-template.svg"
+                    className={styles.brand}
+                    width={80}
+                    height={80}
+                    priority="lazy"
+                    alt="Google"
+                />
+                <div>
+                    <span><strong>Company name</strong></span>
+                    <Link href="" className={styles.company_link}>Company link</Link>
+                </div>
+            </div>
+            <div className={styles.column_wrapper}>
+                <p className={styles.title}>Location</p>
+                <strong>Remote</strong>
+            </div>
+            <div className={styles.column_wrapper}>
+                <p className={styles.title}>Knowedge</p>
+                <strong>Pleno/Senior</strong>
+            </div>
+            <div className={styles.column_wrapper}>
+                <p className={styles.title}>Language</p>
+                <strong>Swift</strong>
+            </div>
+        </div>
+    )
+}
 
 export default function JobList() {
     return (
         <section className={styles.container}>
             <div className={styles.job_list}>
-                <h2>Featured jobs</h2>
-                <div>
-                    <div>Job card</div>
-                    <div>Job card</div>
+                <div className={styles.job_list_items}>
+                    <h2>Featured jobs</h2>
+                    <JobListItem />
+                    <JobListItem />
                 </div>
 
-                <h2>Latest jobs</h2>
-                <div>
-                    <div>Job card</div>
-                    <div>Job card</div>
+                <div className={styles.job_list_items}>
+                    <h2>Latest jobs</h2>
+                    <JobListItem />
+                    <JobListItem />
+                    <JobListItem />
                 </div>
             </div>
         </section>
