6fbae4cc756415b929877f5dfdb29562b4c9ee90
Adriano Souza Costa <adriano.souza@prodoctor.net>
2023-09-06 11:37:02 -0300

    Adding analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from './components/header'
 import Footer from './components/footer'
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Header languages={languages} levels={levels} />
         {children}
         <Footer languages={languages} levels={levels} />
+        <Analytics />
       </body>
     </html>
   )
