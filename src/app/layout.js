a6cb78af607696db6c91a7fb80b13414f7dff98d
Adriano Souza Costa <adriano.souza@prodoctor.net>
2023-09-12 03:31:04 -0300

    Configuring the ad block

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6518499187853251" crossorigin="anonymous"></script>
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-6518499187853251?ers=1" nonce="lUbhwNYVI1QgkEEUh1G9Qg"></script><script nonce="lUbhwNYVI1QgkEEUh1G9Qg">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
       </head>
       <body className={inter.className}>
         <Header languages={languages} levels={levels} />
